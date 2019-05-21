06521b736d3602e62cf7d433bc78ac41f41fd93f
McG <monetka86@gmail.com>
2019-05-21 15:59:12 +0300

    Сервер готов на 50%

--- common/utils.py
+++ common/utils.py
@@ -1,1 +1,26 @@
-1
+from common.variables import *
+import json
+
+
+# Утилита приёма и декодирования сообщения
+# принимает байты выдаёт словарь, если приняточто-то другое отдаёт ошибку значения
+
+def get_message(client):
+    encoded_response = client.recv(MAX_PACKAGE_LENGTH)
+    if isinstance(encoded_response, bytes):
+        json_response = encoded_response.decode(ENCODING)
+        response = json.loads(json_response)
+        if isinstance(response, dict):
+            return response
+        else:
+            raise ValueError
+    else:
+        raise ValueError
+
+
+# Утилита кодирования и отправки сообщения
+# принимает словарь и отправляет его
+def send_message(sock , message):
+    js_message = json.dumps(message)
+    encoded_message = js_message.encode(ENCODING)
+    sock.send(encoded_message)
--- server.py
+++ server.py
@@ -1,37 +1,66 @@
 import socket
 import sys
+import json
 from common.variables import *
 from common.utils import *
 
 
-# Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию.
-
-#Сначала обрабатываем порт:
-try:
-    if '-p' in sys.argv:
-        listen_port = int(sys.argv[sys.argv.index('-p') + 1])
-    else:
-        listen_port = DEFAULT_PORT
-    if listen_port < 1024 or listen_port > 65535:
-        raise ValueError
-except IndexError:
-    print('После параметра \'p\'- необходимо указать номер порта.')
-    exit(1)
-except ValueError:
-    print('В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
-    exit(1)
-
-# Затем загражем какой адрес слушать
-
-try:
-    if '-a' in sys.argv:
-        listen_address = int(sys.argv[sys.argv.index('-a') + 1])
-    else:
-        listen_address = ''
-
-except IndexError:
-    print('После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
-    exit(1)
+# Обработчик сообщений от клиентов, принимает словарь - сообщение от клинта, созвращает словарь ответ для клиента
+def process_client_message(message):
+    return {'test' : 'test'}
 
 
-print(listen_port)
+def main():
+    # Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию.
+
+    # Сначала обрабатываем порт:
+    try:
+        if '-p' in sys.argv:
+            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
+        else:
+            listen_port = DEFAULT_PORT
+        if listen_port < 1024 or listen_port > 65535:
+            raise ValueError
+    except IndexError:
+        print('После параметра \'p\'- необходимо указать номер порта.')
+        exit(1)
+    except ValueError:
+        print('В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
+        exit(1)
+
+    # Затем загружаем какой адрес слушать
+
+    try:
+        if '-a' in sys.argv:
+            listen_address = int(sys.argv[sys.argv.index('-a') + 1])
+        else:
+            listen_address = ''
+
+    except IndexError:
+        print('После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
+        exit(1)
+
+    # Готовим сокет
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.bind((listen_address, listen_port))
+
+    # Слушаем порт
+
+    transport.listen(MAX_CONNECTIONS)
+
+    while True:
+        client , client_address = transport.accept()
+        try:
+            message_from_cient = get_message(client)
+            print(message_from_cient)
+            response = process_client_message(message_from_cient)
+            send_message(client, response)
+            client.close()
+        except (TypeError , json.JSONDecodeError):
+            print('Принято некорретное сообщение от клиента.')
+            client.close()
+
+
+if __name__ == '__main__':
+    main()
