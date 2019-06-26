c3a12799fe12c60754aa06a055668d517be861c0
xxMcGxx <49191089+xxMcGxx@users.noreply.github.com>
2019-06-26 21:58:39 +0300

    Lession4 Done

--- server.py
+++ server.py
@@ -16,9 +16,9 @@ from decos import log
 from descryptors import Port
 from metaclasses import ServerMaker
 from server_database import ServerStorage
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMessageBox
 from PyQt5.QtCore import QTimer
-from server_gui import MainWindow, gui_create_model, HistoryWindow, create_stat_model
+from server_gui import MainWindow, gui_create_model, HistoryWindow, create_stat_model, ConfigWindow
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 
 # Инициализация логирования сервера.
@@ -226,7 +226,7 @@ def main():
     listen_address, listen_port = arg_parser(config['SETTINGS']['Default_port'], config['SETTINGS']['Listen_Address'])
 
     # Инициализация базы данных
-    database = ServerStorage(os.path.join(config['SETTINGS']['Database_path'] , config['SETTINGS']['Database_file']))
+    database = ServerStorage(os.path.join(config['SETTINGS']['Database_path'], config['SETTINGS']['Database_file']))
 
     # Создание экземпляра класса - сервера и его запуск:
     server = Server(listen_address, listen_port, database)
@@ -262,6 +262,38 @@ def main():
         stat_window.history_table.resizeRowsToContents()
         stat_window.show()
 
+    # Функция создающяя окно с настройками сервера.
+    def server_config():
+        global config_window
+        # Создаём окно и заносим в него текущие параметры
+        config_window = ConfigWindow()
+        config_window.db_path.insert(config['SETTINGS']['Database_path'])
+        config_window.db_file.insert(config['SETTINGS']['Database_file'])
+        config_window.port.insert(config['SETTINGS']['Default_port'])
+        config_window.ip.insert(config['SETTINGS']['Listen_Address'])
+        config_window.save_btn.clicked.connect(save_server_config)
+
+    # Функция сохранения настроек
+    def save_server_config():
+        global config_window
+        message = QMessageBox()
+        config['SETTINGS']['Database_path'] = config_window.db_path.text()
+        config['SETTINGS']['Database_file'] = config_window.db_file.text()
+        try:
+            port = int(config_window.port.text())
+        except ValueError:
+            message.warning(config_window, 'Ошибка', 'Порт должен быть числом')
+        else:
+            config['SETTINGS']['Listen_Address'] = config_window.ip.text()
+            if 1023 < port < 65536:
+                config['SETTINGS']['Default_port'] = str(port)
+                print(port)
+                with open('server.ini', 'w') as conf:
+                    config.write(conf)
+                    message.information(config_window , 'OK' , 'Настройки успешно сохранены!')
+            else:
+                message.warning(config_window , 'Ошибка' , 'Порт должен быть от 1024 до 65536')
+
     # Таймер, обновляющий список клиентов 1 раз в секунду
     timer = QTimer()
     timer.timeout.connect(list_update)
@@ -270,6 +302,7 @@ def main():
     # Связываем кнопки с процедурами
     main_window.refresh_button.triggered.connect(list_update)
     main_window.show_history_button.triggered.connect(show_statistics)
+    main_window.config_btn.triggered.connect(server_config)
 
     # Запускаем GUI
     server_app.exec_()
--- server_gui.py
+++ server_gui.py
@@ -1,7 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QLabel, QTableView, QDialog, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QLabel, QTableView, QDialog, QPushButton, \
+    QLineEdit, QFileDialog , QMessageBox
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from PyQt5.QtCore import Qt
+import os
 
 
 # GUI - Создание таблицы QModel, для отображения в окне программы.
@@ -62,6 +64,9 @@ class MainWindow(QMainWindow):
         # Кнопка обновить список клиентов
         self.refresh_button = QAction('Обновить список', self)
 
+        # Кнопка настроек сервера
+        self.config_btn = QAction('Настройки сервера' , self)
+
         # Кнопка вывести историю сообщений
         self.show_history_button = QAction('История клиентов', self)
 
@@ -73,6 +78,7 @@ class MainWindow(QMainWindow):
         self.toolbar.addAction(exitAction)
         self.toolbar.addAction(self.refresh_button)
         self.toolbar.addAction(self.show_history_button)
+        self.toolbar.addAction(self.config_btn)
 
         # Настройки геометрии основного окна
         # Поскольку работать с динамическими размерами мы не умеем, и мало времени на изучение, размер окна фиксирован.
@@ -118,6 +124,89 @@ class HistoryWindow(QDialog):
         self.show()
 
 
+# Класс окна настроек
+class ConfigWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        # Настройки окна
+        self.setFixedSize(365, 260)
+        self.setWindowTitle('Настройки сервера')
+
+        # Надпись о файле базы данных:
+        self.db_path_label = QLabel('Путь до файла базы данных: ', self)
+        self.db_path_label.move(10, 10)
+        self.db_path_label.setFixedSize(240, 15)
+
+        # Строка с путём базы
+        self.db_path = QLineEdit(self)
+        self.db_path.setFixedSize(250, 20)
+        self.db_path.move(10, 30)
+        self.db_path.setReadOnly(True)
+
+        # Кнопка выбора пути.
+        self.db_path_select = QPushButton('Обзор...', self)
+        self.db_path_select.move(275, 28)
+
+        # Функция обработчик открытия окна выбора папки
+        def open_file_dialog():
+            global dialog
+            dialog = QFileDialog(self)
+            path = dialog.getExistingDirectory()
+            path = path.replace('/', '\\')
+            self.db_path.insert(path)
+
+        self.db_path_select.clicked.connect(open_file_dialog)
+
+        # Метка с именем поля файла базы данных
+        self.db_file_label = QLabel('Имя файла базы данных: ', self)
+        self.db_file_label.move(10, 68)
+        self.db_file_label.setFixedSize(180, 15)
+
+        # Поле для ввода имени файла
+        self.db_file = QLineEdit(self)
+        self.db_file.move(200, 66)
+        self.db_file.setFixedSize(150 , 20)
+
+        # Метка с номером порта
+        self.port_label = QLabel('Номер порта для соединений:', self)
+        self.port_label.move(10, 108)
+        self.port_label.setFixedSize(180, 15)
+
+        # Поле для ввода номера порта
+        self.port = QLineEdit(self)
+        self.port.move(200, 108)
+        self.port.setFixedSize(150, 20)
+
+        # Метка с адресом для соединений
+        self.ip_label = QLabel('С какого IP принимаем соединения:', self)
+        self.ip_label.move(10, 148)
+        self.ip_label.setFixedSize(180, 15)
+
+        # Метка с напоминанием о пустом поле.
+        self.ip_label_note = QLabel(' оставьте это поле пустым, чтобы\n принимать соединения с любых адресов.', self)
+        self.ip_label_note.move(10, 168)
+        self.ip_label_note.setFixedSize(500, 30)
+
+        # Поле для ввода ip
+        self.ip = QLineEdit(self)
+        self.ip.move(200, 148)
+        self.ip.setFixedSize(150, 20)
+
+        # Кнопка сохранения настроек
+        self.save_btn = QPushButton('Сохранить' , self)
+        self.save_btn.move(190 , 220)
+
+        # Кнапка закрытия окна
+        self.close_button = QPushButton('Закрыть', self)
+        self.close_button.move(275, 220)
+        self.close_button.clicked.connect(self.close)
+
+        self.show()
+
+
 if __name__ == '__main__':
     '''
     app = QApplication(sys.argv)
@@ -133,7 +222,7 @@ if __name__ == '__main__':
     app.exec_()
     print('END')'''
     app = QApplication(sys.argv)
-
-    dial = HistoryWindow()
+    message = QMessageBox
+    dial = ConfigWindow()
 
     app.exec_()
